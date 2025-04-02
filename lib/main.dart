4984e5a92715bdd0bba2da753c1b5bf12f2e4adc
Durgesh9M <armadilloforwar02@gmail.com>
2025-04-02 12:43:18 +0530

    delete pages and fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:feed_spaces/features/feed_spaces/ui/feed_spaces_screen.dart';
-import 'package:feed_spaces/features/feed_spaces/ui/feeds_page.dart';
-import 'package:feed_spaces/features/feed_spaces/ui/spaces_page.dart';
-import 'package:feed_spaces/features/feed_spaces/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: FeedsPage(),
+      home: FeedSpacesScreen(),
     );
   }
 }
